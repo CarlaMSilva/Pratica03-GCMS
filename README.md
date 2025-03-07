a185e54fdc911a7e39f13dbcb14dd32f2c6b0d0a
CarlaMSilva <carlamariasilva41@outlook.com>
2025-03-07 13:27:21 -0300

    resolvendo bugs

---
+++ README.md
@@ -0,0 +1,6 @@
+# GCMS-Carla-silva
+
+# Colaboradores
+Coloque seu nome abaixo se você é um colaborador do repositório.
+* Carla Maria(@CarlaMSilva)
+* Ramide Dantas (@ramidedantas)
---
+++ src/main/java/br/edu/ifpe/recife/gcms/Pratica02/HomeController.java
@@ -0,0 +1,55 @@
+package br.edu.ifpe.recife.gcms.Pratica02;
+
+<<<<<<< HEAD
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping; 
+	import org.springframework.web.bind.annotation.ResponseBody;
+	
+	@Controller 
+	public class HomeController {
+		
+		//@RequestMapping("/") 
+		//public @ResponseBody String greeting() {
+		//return "Hello, World!";
+		//}
+		
+		@RequestMapping("/carla")
+		public @ResponseBody String greetingCarla() { 
+			return "Hello, Carla Maria teste da prática 3!";
+			}
+		
+		@RequestMapping("/pessoa")
+		public @ResponseBody String greetingPessoa() { 
+			return "Hello, Carla Pessoa da prática 3!";
+			}
+		
+		@RequestMapping("/hotfix")
+		public @ResponseBody String greetingHotfix() { 
+			return "Hello, criação da branch hotfix!";
+			}
+	
+=======
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, world!";
+>>>>>>> a7ed95e8d384f1aa05bf5063a817fd6cb8d3d853
+	}
+
+	@RequestMapping("/carla")
+	public @ResponseBody String greetingCarla() {
+		return "Hello, Carla Maria!";
+	}
+
+	@RequestMapping("/pessoa")
+	public @ResponseBody String greetingPessoa() {
+		return "Hello, Carla Pessoa!";
+	}
+
+}
